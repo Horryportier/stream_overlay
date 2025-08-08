870fba0427523628b73d0ba3c20b74e5c41cb403
horry <horry@pop-os.localdomain>
2025-08-08 03:48:15 +0200

    fixed palette offset

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,4 +1,16 @@
 extends Control
 
+@onready var sun: DirectionalLight2D = %Sun
+@onready var sun_timer: Timer = %SunTimer
+
+@export var sun_color_over_time: Gradient
+
+
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.TURQUOISE)
+
+
+func _process(_delta: float) -> void:
+	var sun_timer_noramlized: float = remap(sun_timer.time_left, 0, sun_timer.wait_time, 0, 1)
+	sun.rotation = -remap(sun_timer_noramlized, 0, 1 ,  0, PI * 2)
+	sun.color = sun_color_over_time.sample(sun_timer_noramlized)
